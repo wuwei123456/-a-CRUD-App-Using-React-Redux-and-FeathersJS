34eebb76696979e4dacec84a936e9834729f9c21
wei.wu <wei.wu@bkjk.com>
2018-12-21 17:27:29 +0800

    react-redux-feathers

---
+++ src/actions/contact-action.js
@@ -0,0 +1,65 @@
+// import { contacts } from '../contact-data';
+
+// export function fetchContacts(){
+//   return dispatch => {
+//     dispatch({
+//       type: 'FETCH_CONTACTS',
+//       payload: contacts
+//     })
+//   }
+// }
+
+import { client } from './';
+
+const url = '/contacts';
+
+export function fetchContacts(){
+  return dispatch => {
+    dispatch({
+      type: 'FETCH_CONTACTS',
+      payload: client.get(url)
+    })
+  }
+}
+export function fetchContact(_id) {
+  return dispatch => {
+    return dispatch({
+      type: 'FETCH_CONTACT',
+      payload: client.get(`${url}/${_id}`)
+    })
+  }
+}
+export function newContact() {
+  return dispatch => {
+    dispatch({
+      type: 'NEW_CONTACT'
+    })
+  }
+}
+
+export function saveContact(contact) {
+  return dispatch => {
+    return dispatch({
+      type: 'SAVE_CONTACT',
+      payload: client.post(url, contact)
+    })
+  }
+}
+
+export function updateContact(contact) {
+  return dispatch => {
+    return dispatch({
+      type: 'UPDATE_CONTACT',
+      payload: client.put(`${url}/${contact._id}`, contact)
+    })
+  }
+}
+
+export function deleteContact(_id){
+  return dispatch => {
+    return dispatch({
+      type:'DELETE_CONTACT',
+      payload:client.delete(`${url}/${_id}`)
+    })
+  }
+}
